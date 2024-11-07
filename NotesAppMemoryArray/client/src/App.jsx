4ac0edce73ff8c15d0a1af20d867a59b2090c480
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-07 22:50:09 +0200

    Update App.jsx

--- NotesAppMemoryArray/client/src/App.jsx
+++ NotesAppMemoryArray/client/src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./routes/HomePage.jsx";
+import CreatePage from "./routes/CreatePage.jsx";
+import UpdatePage from "./routes/UpdatePage.jsx";
+import SinglePage from "./routes/SinglePage.jsx";
+
 function App() {
-  
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/update/:id" element={<UpdatePage />} />
+        <Route path="/single/:id" element={<SinglePage />} />
+      </Routes>
     </div>
   );
 }
