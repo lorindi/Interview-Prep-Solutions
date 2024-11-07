c514367ef61f466f4496208a65bad1ad11a31fc2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-07 22:50:18 +0200

    Create HomePage.jsx

---
+++ NotesAppMemoryArray/client/src/routes/HomePage.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { allNotes } from "../services/notesService.js";
+import Card from "../components/Card.jsx";
+
+function HomePage() {
+  const [notes, setNotes] = useState([]);
+  console.log(notes);
+  
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    try {
+      const res = await allNotes();
+      const data = res.data.notes;
+      setNotes(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      {notes.length > 0 ? (
+        notes.map((note) => <Card note={note} key={note.id}/>)
+      ) : (
+        <p>No notes available</p>
+      )}
+    </div>
+  );
+}
+
+export default HomePage;
