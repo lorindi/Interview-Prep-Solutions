b0071f801f28917800e8d2ac2e87141b3ba294de
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-07 21:07:28 +0200

    Create noterService.js

---
+++ NotesAppMemoryArray/client/src/services/noterService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/notes";
+
+export const createNote = async (newData) => {
+  return axios.post(`${API_URL}/create`);
+};
+export const updateNote = async (id, updateData) => {
+  return axios.put(`${API_URL}/update/${id}`);
+};
+export const deleteNote = async (id) => {
+  return axios.delete(`${API_URL}/delete/${id}`);
+};
+export const allNotes = async () => {
+  return axios.get(`${API_URL}/all`);
+};
+export const singleNote = async (id) => {
+  return axios.get(`${API_URL}/note/${id}`);
+};
