d0bae72caaa224f6470e877bf225d8302b787aab
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-07 20:50:14 +0200

    Update app.js

--- NotesAppMemoryArray/server/app.js
+++ NotesAppMemoryArray/server/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
 
 app.get('/', (req, res) => {
     res.send('Restful server');
