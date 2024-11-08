939aa83b17e42c84bc90ebac1407b0ac6dc207f5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-08 13:32:27 +0200

    Update HomePage.jsx

--- NotesAppMemoryArray/client/src/routes/HomePage.jsx
+++ NotesAppMemoryArray/client/src/routes/HomePage.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { allNotes } from "../services/notesService.js";
 import Card from "../components/Card.jsx";
+import CreatePage from "./CreatePage.jsx";
 
 function HomePage() {
   const [notes, setNotes] = useState([]);
   console.log(notes);
-  
 
   useEffect(() => {
     fetchNotes();
@@ -23,8 +23,9 @@ function HomePage() {
 
   return (
     <div>
+      <CreatePage fetchNotes={fetchNotes}/>
       {notes.length > 0 ? (
-        notes.map((note) => <Card note={note} key={note.id}/>)
+        notes.map((note) => <Card note={note} key={note.id} />)
       ) : (
         <p>No notes available</p>
       )}
