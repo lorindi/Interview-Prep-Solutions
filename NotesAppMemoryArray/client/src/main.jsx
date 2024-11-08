06a84fc57428353103685ace25ebdcc5e7de5d94
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-08 13:45:39 +0200

    Update main.jsx

--- NotesAppMemoryArray/client/src/main.jsx
+++ NotesAppMemoryArray/client/src/main.jsx
@@ -3,11 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
     <StrictMode>
       <App />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+  
+      />
     </StrictMode>
   </BrowserRouter>
   ,
