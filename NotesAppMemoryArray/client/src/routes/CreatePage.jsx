eda756225e5297eead4ce99e1039448330a677f3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-08 13:32:25 +0200

    Update CreatePage.jsx

--- NotesAppMemoryArray/client/src/routes/CreatePage.jsx
+++ NotesAppMemoryArray/client/src/routes/CreatePage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { createNote } from "../services/notesService";
+function CreatePage({fetchNotes}) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-function CreatePage() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createNote({ title, description });
+      fetchNotes()
+      setTitle("");
+      setDescription("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div>CreatePage</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">Create</button>
+    </form>
+  );
 }
 
-export default CreatePage
+export default CreatePage;
